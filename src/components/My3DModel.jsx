80e2a9ba62214d08e5a74bc8094a305664e112d9
ronin1777 <hoseincomiser27@gmail.com>
2025-08-28 20:55:28 +0300

    change size of model

--- src/components/My3DModel.jsx
+++ src/components/My3DModel.jsx
@@ -1,25 +1,27 @@
 import { OrbitControls, useGLTF } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import React, { Suspense } from "react";
+import { Suspense } from "react";
+import { DRACOLoader } from "three-stdlib";
+
+// یک loader مشترک برای DRACO بساز
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath("/draco/"); // مسیر public/draco/
 
 export function My3DModel() {
-  const { scene } = useGLTF("/guts.glb");
+  // useGLTF با loader مخصوص DRACO
+  const { scene } = useGLTF("/guts-draco.glb", true, dracoLoader);
   return <primitive object={scene} scale={1} position={[0, -1.5, 0]} />;
 }
+
 export default function ModelViewer() {
   return (
-    <div
-      style={{
-        width: "100%px",
-        height: "400px",
-      }}
-    >
+    <div style={{ width: "100%", height: "400px" }}>
       <div className="model-wrapper">
         <Canvas camera={{ position: [0, 1, 8], fov: 50 }}>
           <ambientLight intensity={1} />
           <directionalLight position={[2, 2, 2]} intensity={1} />
           <Suspense fallback={null}>
-            <My3DModel position={[0, -0.5, 0]} />
+            <My3DModel />
           </Suspense>
           <OrbitControls enableZoom={false} />
         </Canvas>
